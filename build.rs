b6864684a5511bc212fdceb40a4d044d7c88afee
Enrico Lumetti <enrico.lumetti@gmail.com>
2017-08-07 15:10:13 +0200

    First commit

---
+++ build.rs
@@ -0,0 +1,41 @@
+extern crate bindgen;
+
+use std::process::Command;
+use std::env;
+use std::path::{PathBuf};
+
+fn main() {
+    let out_dir = env::var("OUT_DIR").unwrap();
+
+    // note that there are a number of downsides to this approach, the comments
+    // below detail how to improve the portability of these commands.
+    let cc_args = vec!["tga/tgaimage.cpp", "-g", "-shared", "-rdynamic", "-fPIC", "-o"];
+    Command::new("g++").args(cc_args.as_slice())
+                       .arg(&format!("{}/libtgaimage.so", out_dir))
+                       .status().unwrap();
+
+
+    println!("cargo:rustc-link-search=native={}", out_dir);
+    println!("cargo:rustc-link-lib=tgaimage");
+
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header("tga/tgaimage.hpp")
+        .whitelisted_type("TGAImage")
+        .whitelisted_type("TGAColor")
+        //.conservative_inline_namespaces() // see issue #789
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
---
+++ src/main.rs
@@ -0,0 +1,30 @@
+mod tga;
+
+use tga::{TGAImage, TGAImage_Format, TGAColor};
+use std::ffi::CString;
+use std::os::raw::*;
+
+/*
+#include "tgaimage.hpp"
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red   = TGAColor(255, 0,   0,   255);
+int main(int argc, char** argv) {
+        TGAImage image(100, 100, TGAImage::RGB);
+        image.set(52, 41, red);
+        image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+        image.write_tga_file("output.tga");`
+        return 0;
+}
+*/
+
+fn main() {
+    unsafe {
+        let mut image = TGAImage::new1(100, 100, TGAImage_Format::RGB as c_int);
+        let color = TGAColor::new1(255, 0, 0, 255);
+        image.set(52, 41, color);
+        image.flip_vertically();
+        image.write_tga_file(CString::new("output.tga").unwrap().as_ptr(), true);
+        image.destruct();
+    }
+    println!("Finished writing to output.tga");
+}
